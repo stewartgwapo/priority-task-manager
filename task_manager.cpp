e8206d1bea56e64875a30ef9fdce5fb8f1962b5d
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-07-01 21:08:56 +0530

    Initial commit

---
+++ task_manager.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+using namespace std;
+
+class Task {
+    public:
+    int priority;
+    string taskName;
+    string taskDesc;
+    string date;
+    bool completed;
+};
+
+class TaskManager {
+    void addTask();
+    void removeTask();
+};
+
+int main() {
+    
+    return 0;
+}
