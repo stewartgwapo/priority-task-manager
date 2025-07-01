780bcdf60416d53f1853ee967e102a0e795128e3
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-07-01 21:33:04 +0530

    Add welcome page display and task manager menu

--- task_manager.cpp
+++ task_manager.cpp
@@ -18,7 +18,26 @@ class TaskManager {
     void removeTask();
 };
 
+void printWelcomePage() {
+    cout << "=================================================================\n";
+    cout << "| ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ Priority Task Manager ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |\n";
+    cout << "=================================================================\n";
+    cout << "| ~ ~ ~ ~ ~ ~ ~ ~ Manage your tasks efficiently ~ ~ ~ ~ ~ ~ ~ ~ |\n";
+    cout << "=================================================================\n";
+    cout << "What's your first name? "; string fname; cin>>fname;
+    cout << "\nWhat would you like to do, " << fname << "?\n";
+    cout << "1. Add Task\n";
+    cout << "2. Update Task\n";
+    cout << "3. Mark Task as Completed\n";
+    cout << "4. Show All Tasks\n";
+    cout << "5. Remove Task\n";
+    cout << "6. Clear All Tasks\n";
+    cout << "0. Exit\n";
+    cout << "=================================================================\n";
+}
+
 int main() {
+    printWelcomePage();
     
     return 0;
 }
